5ddd733f154df2fcaa965b3c9bf1da889072b9a4
watersir <1057864218@qq.com>
2022-02-07 08:39:13 +0800

    p

--- segment.c
+++ segment.c
@@ -1538,7 +1538,7 @@ static int __issue_discard_cmd(struct f2fs_sb_info *sbi,
 				// long long int ssd_invalid = remapSendor(0,0);
 				// long long int ssd_total = 4718592;
 				// int k = 32;
-				if(dc->len < 64) { // i means the block size, not sector size.
+				if(i < 128) { // i means the block size, not sector size.
 					io_interrupted = true;
 					break;
 				}
